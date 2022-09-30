a7b1a1161a52d4ac90f297a4e54bc74e7b95339e
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-09-30 17:43:20 -0500

    Navbar height fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import { FiExternalLink } from "react-icons/fi"
 import { Link } from 'react-scroll';
-import { BsChevronDown } from "react-icons/bs"
+import { BsChevronDown, BsPersonLinesFill } from "react-icons/bs"
 
 
 const Home = () => {
@@ -8,7 +8,7 @@ const Home = () => {
     return (
         <div name="Home" className="h-screen w-full bg-gradient-to-b from-black via-black to-fifth">
 
-            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-10 sm:py-0 sm:px-4">
+            <div className="max-w-screen-lg mx-auto flex items-center justify-center h-full px-10 sm:py-0 sm:px-4">
 
                 <div className="flex flex-col justify-center gap-2 sm:gap-4 h-full">
 
@@ -40,6 +40,11 @@ const Home = () => {
                         <a href="https://moviehome.akhilkumar.ga/" target="_blank" rel="noreferrer" className="text-[#ABDF12] font-slogan border-2 border-[#ABDF12] my-2 px-6 py-3 rounded-md flex items-center gap-1 cursor-pointer duration-500 hover:bg-[#ABDF12]/20">Check out my latest Project<FiExternalLink size={18} />
                         </a>
 
+                        <a href="/Resume-AkhilKumar.pdf" target="_blank" rel="noreferrer">
+                            <BsPersonLinesFill size={24} className="duration-300 hover:text-primary hover:scale-105" />
+                        </a>
+
+
                     </div>
                     <div className="flex justify-center"
                         data-aos="fade-up"
@@ -52,9 +57,11 @@ const Home = () => {
 
                 </div>
 
+
+
             </div>
 
-        </div>
+        </div >
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,9 @@ import logo from '../assets/logo.svg';
 import { CgMenu, CgClose } from "react-icons/cg";
 import { useState } from 'react';
 import { Link } from 'react-scroll';
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { BsPersonLinesFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
 
 const Navbar = () => {
 
@@ -31,57 +34,68 @@ const Navbar = () => {
     const [menu, setMenu] = useState(false);
 
     return (
-        <div className="flex justify-center items-center w-full h-20 text-white backdrop-blur bg-black/70
+        <>
+            <div className="flex justify-center items-center w-full h-12 text-secondary backdrop-blur bg-black/70
         px-4 fixed shadow-xl z-50">
-            <div className="max-w-screen-lg flex justify-between items-center w-full">
-                <Link to="Home" smooth duration={500}>
-                    <img className="w-20 cursor-pointer hover:scale-95 duration-200" src={logo} alt="Logo"
-                        data-aos="fade-up-left"
-                        data-aos-once="true"
-                        data-aos-delay="0"></img>
-                </Link>
+                <div className="max-w-screen-lg flex justify-between items-center w-full">
+                    <Link to="Home" smooth duration={500}>
+                        <img className="w-14 cursor-pointer hover:scale-95 duration-200" src={logo} alt="Logo"
+                            data-aos="fade-up-left"
+                            data-aos-once="true"
+                            data-aos-delay="0"></img>
+                    </Link>
 
 
-                <ul className="hidden md:flex">
-                    {items.map(({ id, item, duration }) => (
-                        <>
-                            <li key={id} className="px-4 cursor-pointer text-secondary hover:text-primary"
-                                data-aos="fade-down"
-                                data-aos-once="true"
-                                data-aos-delay={duration}>
-                                <Link to={item} smooth duration={500}>
-                                    <a key={id} href={item} className="text-primary pr-2">{id}</a>
-                                    {item}
-                                </Link>
-                            </li>
-                        </>
-                    ))}
-                </ul>
+                    <ul className="hidden md:flex gap-5">
+                        {items.map(({ id, item, duration }) => (
+                            <>
+                                <li key={id} className="cursor-pointer text-secondary duration-300 hover:text-primary"
+                                    data-aos="fade-down"
+                                    data-aos-once="true"
+                                    data-aos-delay={duration}>
+                                    <Link to={item} smooth duration={500}>
+                                        {item}
+                                    </Link>
+                                </li>
+                            </>
+                        ))}
+                        <div className="flex gap-1">
+                            <a href="https://github.com/AkhilsWorkshop" target="_blank" rel="noreferrer">
+                                <AiFillGithub size={25} className="duration-300 hover:text-primary hover:scale-110" />
+                            </a>
 
-                <div onClick={() => setMenu(!menu)} className="cursor-pointer z-20 text-secondary md:hidden">
-                    {menu ? <CgClose size={35} /> : <CgMenu size={35} />}
+                            <a href="https://www.linkedin.com/in/akhilkumarh/" target="_blank" rel="noreferrer">
+                                <AiFillLinkedin size={25} className="duration-300 hover:text-primary hover:scale-110" />
+                            </a>
+                        </div>
+                    </ul>
+
+                    <div onClick={() => setMenu(!menu)} className="cursor-pointer z-20 text-secondary md:hidden">
+                        {menu ? <CgClose size={35} /> : <CgMenu size={35} />}
+                    </div>
+
+                    {menu && (
+                        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black via-sixth to-black">
+                            {items.map(({ id, item }) => (
+                                <li key={id} className="px-4 cursor-pointer text-secondary py-6 text-4xl">
+                                    <Link onClick={() => setMenu(!menu)} to={item} smooth duration={500}>
+                                        {item}
+                                    </Link>
+                                </li>
+                            ))}
+                            <a href="/Resume-AkhilKumar.pdf" target="_blank" rel="noreferrer" className="text-secondary w-fit px-6 py-3 my-6 border-2 border-primary flex items-center rounded-md">Resume
+                            </a>
+                        </ul>
+                    )}
+
+
                 </div>
 
-                {menu && (
-                    <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black via-sixth to-black">
-                        {items.map(({ id, item }) => (
-                            <li key={id} className="px-4 cursor-pointer text-secondary py-6 text-4xl">
-                                <Link onClick={() => setMenu(!menu)} to={item} smooth duration={500}>
-                                    {item}
-                                </Link>
-                            </li>
-                        ))}
-                        <a href="/Resume-AkhilKumar.pdf" target="_blank" rel="noreferrer" className="text-secondary w-fit px-6 py-3 my-6 border-2 border-primary flex items-center rounded-md">Resume
-                        </a>
-                    </ul>
-                )}
 
 
             </div>
 
-
-
-        </div>
+        </>
     )
 }
 
